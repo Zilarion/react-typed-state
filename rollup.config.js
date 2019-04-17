29fa39a62968484a138bd2ffe2cd00b279d10ecc
Ruud Andriessen <ruud.andriessen@gmail.com>
2019-04-17 21:22:22 +0200

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
-const name = "UseState";
+const name = 'UseState'
 
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   plugins: [
     // Allows node_modules resolution
     resolve({ extensions }),
@@ -17,25 +17,25 @@ export default {
     commonjs(),
 
     // Compile TypeScript/JavaScript files
-    babel({ extensions, include: ["src/**/*"] })
+    babel({ extensions, include: ['src/**/*'] })
   ],
 
   output: [
     {
       file: pkg.main,
-      format: "cjs"
+      format: 'cjs'
     },
     {
       file: pkg.module,
-      format: "es"
+      format: 'es'
     },
     {
       file: pkg.browser,
-      format: "iife",
+      format: 'iife',
       name,
 
       // https://rollupjs.org/guide/en#output-globals-g-globals
       globals: {}
     }
   ]
-};
+}
